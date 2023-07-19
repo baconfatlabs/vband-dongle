c2237c287289be79c8800596714ec077fd8df800
Eric Wolf <eric@baconfatlabs.com>
2023-07-19 16:40:21 -0500

    Initial project commit

---
+++ README.md
@@ -0,0 +1,56 @@
+#Ham Radio Solutions - VBand Dongle Clone
+
+I created this just for fun, as a tinkerer I took a look at the most prevalent
+DIY solution for this that uses an Arduino Pro Micro.  I highly recommend the
+actual solution on the Ham Radio Solutions - https://hamradio.solutions/vband/
+
+Unless you're just itching to tinker or looking to do a ham club project, that
+design is only 25 bucks and VERY reasonably priced, you probably can't build
+this for that much in small quantities.  Go support those guys,
+they're amazing and notable supporters of the DIY community in kind.
+
+Now, if I haven't dissuaded you from rolling your own, a few things:
+
+##The Hardware:
+This project includes a BOM, which is really just a specific TRS jack other
+than the board, Arduino Pro Micro, a 90 degree 2 pin header (which really is
+largely unnecessary), and that's about it.
+
+Also included are gerber files that you can use to submit to any board house of
+your choice, and pictures for reference.  I use OSHPark for my prototyping and
+you can find the shared project for sale here if you just want to order.  I get
+NOTHING from those sales, and I'm totally fine with that!
+https://oshpark.com/shared_projects/HH9dUUS9
+![Board Rendering Top](board_outline_top.png)
+![Board Rendering Top](board_outline_bottom.png)
+
+##The Schematic
+Nothing special.  Follow the goes-into's and goes-outta's and you'll be fine.
+![Electrical Schematic](keyer_schematic.png)
+
+##The Software:
+Adapted without shame from a design by OZ1JHM found here -
+http://oz1jhm.dk/content/hamradio-solutions-vband-interface
+
+I had a few problems with their implementation, albeit a good start.  After
+looking at the code, I noticed that every loop was causing the keyboard to send
+a releaseAll(), which was happening about every 5ms according to the delay
+timer.  This, even though a working design, caused the TX light on the Pro Micro
+to shine constantly, and I may still have a spot in my vision from the bright
+light it generated.  It also caused some issues with attempting to place new
+code onto the Arduino.  I had to force it to bootstrap mode because it tied up
+the USB interface so much that I could not place code onto the chip.
+
+The resulting design I came up with uses a global state approach, only sending
+keyboard traffic when one of the states of the inputs change, and evaluating the
+logic accordingly based on those states.  This is a MUCH "quieter" loop, and the
+TX LED only comes on when a control key is either pressed or released.  As I
+stated earlier, the header pin is largely deprecated in this design I believe.
+You can set VBand to either it's keyer function, or a straight key, which would
+in turn allow you to use either an external keyer or a straight key without
+modification.  It may eventually be eliminated from subsequent designs but it's
+not hurting anything.  Feel free to omit if you want to save 3 cents.
+
+If you modify this firmware, I'd love it if you would create a fork, and send me
+a pull request.  I'll track it on a separate branch and, who knows, if it's
+super useful I'll pull it into main.
---
+++ src/hrs_vband_interface/hrs_vband_interface.ino
@@ -0,0 +1,113 @@
+#include <Keyboard.h>
+
+//***********************************************************************************************************************
+// Author: Eric Wolf, N5EBW <eric.n5ebw@gmail.com>
+// Date: 07/19/2023
+// Original inspiration by OZ1JHM 05/02/2021
+// Description: This program is for use with Ham Radio Solutions VBand Interface.  It interprets open/close signals from a paddle,
+//    straight key, or keyer input into Left Control/Right Control key presses and releases that VBand recognizes.
+
+// Prerequisites: In Arduino, search for "keyboard" in the library manager and install.
+
+// Instructions: 
+//    Paddle: Don't connect MODE_PIN to GND.
+//    Keyer Circuit: Connect MODE_PIN to GND.
+//    Straight Key: Connect MODE_PIN to GND.
+// *** This is really not even necessary if you are connecting to VBand unless you have something miswired I don't think.
+//     If you put VBand in straight key mode and leave this as is, as long as you wire your output to ring, this will still
+//     work without modification in configuration.
+
+// Notes: Remember to set the settings on https://hamradio.solutions/vband
+
+#include <Keyboard.h>
+
+// Define pins on the Arduino that map to the device this is installed on.
+# define DIH_PIN 3
+# define DAH_PIN 4
+# define MODE_PIN 2
+
+// Track the historic state of both pins
+bool DIH_PRESSED;
+bool DAH_PRESSED;
+
+int reaction_delay;
+  
+void setup() {
+  // Set the pin modes for all three inputs.
+  pinMode(DIH_PIN, INPUT_PULLUP);
+  pinMode(DAH_PIN, INPUT_PULLUP);
+  pinMode(MODE_PIN, INPUT_PULLUP);
+
+  // Initially set the working state of both paddle contacts to unpressed.  The loop will determine and set these based on state changes.
+  DIH_PRESSED = false;
+  DAH_PRESSED = false;
+
+  // Reaction delay in between scans for crude debouncing.
+  // The fastest human reaction time is about 120 milliseconds, this should be plenty fast enough to scan without interfering.
+  reaction_delay = 20;
+
+  // Start the keyboard interface where keys will be sent.
+  Keyboard.begin();
+
+}
+
+void loop() {
+  if(digitalRead(MODE_PIN) == HIGH){
+    Mode_keyer();
+  }else{
+    Mode_key();
+  }
+}
+
+// Compare the state of last know pin to the current state.  If there is a change, press a key or release it and set the
+// new global state for that pin for the next loop run. 
+void Mode_keyer(){
+  
+  if(digitalRead(DIH_PIN) == LOW && !DIH_PRESSED){
+    // User has closed the DIH paddle side.  Press the left control key to simulate a dih, set the dih state to pressed.
+    Keyboard.press(KEY_LEFT_CTRL);
+    DIH_PRESSED = true;
+  }
+
+  if(digitalRead(DIH_PIN) == HIGH && DIH_PRESSED){
+    // User has opened the DIH paddle side.  Release the left control key, set the dih state to unpressed.
+    Keyboard.release(KEY_LEFT_CTRL);
+    DIH_PRESSED = false;
+  } 
+
+  if(digitalRead(DAH_PIN) == LOW && !DAH_PRESSED){
+    // User has closed the DAH paddle side.  Press the right control key to simulate a dah, set the dah state to pressed.
+    Keyboard.press(KEY_RIGHT_CTRL);
+    DAH_PRESSED = true;
+  }
+
+  if(digitalRead(DAH_PIN) == HIGH && DAH_PRESSED){
+    // User has opened the DAH paddle side.  Release the right control key, set the dah state to unpressed.
+    Keyboard.release(KEY_RIGHT_CTRL);
+    DAH_PRESSED = false;
+  }
+  delay(reaction_delay);
+}
+
+// Do the same thing we did for keyer mode but only look at the right ctrl key operation.
+// Key can be wired on TIP or RING for this to work as long as one of these pins are pulling low,
+// and both are high upon release.
+// VBand does not seem as stable with the left control key in this mode.
+void Mode_key(){
+  if(digitalRead(DIH_PIN) == LOW && !DAH_PRESSED){
+    // User has closed the key.  Press the right control key, set the dah state to pressed.
+    Keyboard.press(KEY_RIGHT_CTRL);
+    DAH_PRESSED = true;
+  }
+  if(digitalRead(DAH_PIN) == LOW && !DAH_PRESSED){
+    // User has closed the key.  Press the right control key, set the dah state to pressed.
+    Keyboard.press(KEY_RIGHT_CTRL);
+    DAH_PRESSED = true;
+  }
+  if((digitalRead(DAH_PIN) == HIGH && digitalRead(DIH_PIN) == HIGH) && DAH_PRESSED){
+    // User has opened the key.  Release the right control key, set the dah state to unpressed.
+    Keyboard.release(KEY_RIGHT_CTRL);
+    DAH_PRESSED = false;
+  }
+  delay(reaction_delay);
+}
